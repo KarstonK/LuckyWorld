c8fd4ff8d562a29d0a65c094a2dfab708e878bf5
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-04-21 14:39:36 -0400

    Added a bit of  replacing parts {material}_equip

--- Generate.sh
+++ Generate.sh
@@ -7,6 +7,17 @@ cp data/datapack build/datapack
 
 for file in build/datapack/LuckyWorld/data/minecraft/loot_tables/*
 do
-	echo "Processing $file"
+  echo "Processing $file"
   sed -i 's/old-text/new-text/g' $file
+done
+
+for material in materials
+do
+  echo "Processing $material"
+  cp "build/datapack/LuckyWorld/data/lucky_loot/loot_tables/{material}_equip.json" "build/datapack/LuckyWorld/data/lucky_loot/loot_tables/${material}_equip.json"
+  file="build/datapack/LuckyWorld/data/lucky_loot/loot_tables/${material}_equip.json"
+  sed -i "s/{tool_material}/${material[1]}/g" $file
+  sed -i "s/{armor_material}/${material[2]}/g" $file
+  sed -i "s/{level}/${material[3]}/g" $file
+  sed -i "s/{chance}/${material[4]}/g" $file
 done

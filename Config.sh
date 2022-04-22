2c957d891076703f38132f617ce9e488e458f1f1
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-04-22 10:18:00 -0400

    Added to config.sh
    Added documentation, started support for custom items, and for custom models

--- Config.sh
+++ Config.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-equip_materials=(wood_equip stone_equip hard_stone_equip harder_stone_equip iron_equip soft_iron_equip gold_equip diamond_equip)
-wood_equip=(wood wooden leather 2 0.1)
-stone_equip=(stone stone chain 3 0.2)
-hard_stone_equip=(deepslate stone chain 4 0.2)
-harder_stone_equip=(obsidian stone chain 5 0.2)
-iron_equip=(iron iron iron 4 0.3)
-soft_iron_equip=(copper iron iron 3 0.3)
-gold_equip=(gold gold gold 4 0.4)
-diamond_equip=(diamond diamond diamond 5 0.5)
+equip_materials=(wood_equip stone_equip deepslate_equip obsidian_equip iron_equip copper_equip gold_equip diamond_equip)
+# format: {file name}=({tool type} {tool custom model} {armor type} {armor custom model} {enchant level} {enchant chance})
+wood_equip=(wooden 0 leather wood 2 0.1)
+stone_equip=(stone 0 chain stone 3 0.2)
+deepslate_equip=(stone deepslate chain deepslate 4 0.2)
+obsidian_equip=(stone obsidian chain obsidian 5 0.2)
+iron_equip=(iron 0 iron 4 0.3)
+copper_equip=(iron copper iron copper 3 0.3)
+gold_equip=(gold 0 gold 0 4 0.4)
+diamond_equip=(diamond 0 diamond 0 5 0.5)
+
+custom_items=(kb_stick)
+# format: {item name}=({item} {custom model} {custom name} {enchantments})
+# example=(bow minecraft:blocks/dirt 'Example item' 'minecraft:sharpness:10 minecraft:infinity:1 minecraft:power:10')
+kb_stick=(stick 0 'Knockback Stick' minecraft:knockback:10)
 
 lots_weight=( 48 12 3  1)
 lots_quality=(-8 5  2  1)
@@ -17,22 +23,22 @@ extra_weight=64
 extra_quality=16
 extra_count=1
 
-#blocks=(oak_log spruce_log birth_log jungle_log acacia_log dark_oak_log crimson_stem warped_stem stone deepslate obsidian iron_ore copper_ore gold_ore \
-#        diamond_ore)
+blocks=(oak_log spruce_log birth_log jungle_log acacia_log dark_oak_log crimson_stem warped_stem stone deepslate obsidian iron_ore copper_ore gold_ore \
+        diamond_ore)
 # format: {name}=({type(0,1)}{weight(lots,extra)}{item})
-blocks=(diamond_ore iron_ore)
-oak_log=
-spruce_log=
-birth_log=
-jungle_log=
-acacia_log=
-dark_oak_log=
-crimson_stem=
-warped_stem=
-stone=
-deepslate=
-obsidian=
-iron_ore=(0 lots diamond)
-copper_ore=
-gold_ore=
-diamond_ore=(0 lots diamond)
+# example=(0 lots dirt 1 extra minecraft:burried_treasure)
+oak_log=(0 lots oak_log 1 extra lucky_equip:iron_equip)
+spruce_log=(0 lots spruce_log 1 extra lucky_equip:iron_equip)
+birth_log=(0 lots birth_log 1 extra lucky_equip:iron_equip)
+jungle_log=(0 lots jungle_log 1 extra lucky_equip:iron_equip)
+acacia_log=(0 lots acacia_log 1 extra lucky_equip:iron_equip)
+dark_oak_log=(0 lots dark_oak_log 1 extra lucky_equip:iron_equip)
+crimson_stem=(0 lots crimson_stem 1 extra lucky_equip:iron_equip)
+warped_stem=(0 lots warped_stem 1 extra lucky_equip:iron_equip)
+stone=(0 lots cobblestone 1 extra lucky_equip:iron_equip)
+deepslate=(0 lots cobbled_deepslate 1 extra lucky_equip:iron_equip)
+obsidian=(0 lots obsidian 1 extra lucky_equip:iron_equip)
+iron_ore=(0 lots iron_ingot 1 extra lucky_equip:iron_equip)
+copper_ore=(0 lots copper_ingot 1 extra lucky_equip:soft_iron_equip)
+gold_ore=(0 lots gold_ingot 1 extra lucky_equip:gold_equip)
+diamond_ore=(0 lots diamond 1 extra lucky_equip:diamond_equip)

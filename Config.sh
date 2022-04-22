5df6de7bedce0f389d99b5f0a572b75f69cd049c
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-04-22 09:28:19 -0400

    Updated files a lot

--- Config.sh
+++ Config.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-equip_materials=(wood_equip, stone_equip, hard_stone_equip, harder_stone_equip, iron_equip, soft_iron_equip, gold_equip, diamond_equip)
-wood_equip=("wood", "wooden", "leather", 2, 0.1)
-stone_equip=("stone", "stone", "chain", 3, 0.2)
-hard_stone_equip_equip=("deepslate", "stone", "chain", 4, 0.2)
-harder_stone_equip_equip=("obsidian", "stone", "chain", 5, 0.2)
-iron_equip=("iron", "iron", "iron", 4, 0.3)
-soft_iron_equip=("copper", "iron", "iron", 3, 0.3)
-gold_equip=("gold", "gold", "gold", 4, 0.4)
-diamond_equip=("diamond", "diamond", "diamond", 5, 0.5)
+equip_materials=(wood_equip stone_equip hard_stone_equip harder_stone_equip iron_equip soft_iron_equip gold_equip diamond_equip)
+wood_equip=(wood wooden leather 2 0.1)
+stone_equip=(stone stone chain 3 0.2)
+hard_stone_equip=(deepslate stone chain 4 0.2)
+harder_stone_equip=(obsidian stone chain 5 0.2)
+iron_equip=(iron iron iron 4 0.3)
+soft_iron_equip=(copper iron iron 3 0.3)
+gold_equip=(gold gold gold 4 0.4)
+diamond_equip=(diamond diamond diamond 5 0.5)
+
+lots_weight=( 48 12 3  1)
+lots_quality=(-8 5  2  1)
+lots_count=(  1  4  16 64)
+extra_weight=64
+extra_quality=16
+extra_count=1
+
+#blocks=(oak_log spruce_log birth_log jungle_log acacia_log dark_oak_log crimson_stem warped_stem stone deepslate obsidian iron_ore copper_ore gold_ore \
+#        diamond_ore)
+# format: {name}=({type(0,1)}{weight(lots,extra)}{item})
+blocks=(diamond_ore iron_ore)
+oak_log=
+spruce_log=
+birth_log=
+jungle_log=
+acacia_log=
+dark_oak_log=
+crimson_stem=
+warped_stem=
+stone=
+deepslate=
+obsidian=
+iron_ore=(0 lots diamond)
+copper_ore=
+gold_ore=
+diamond_ore=(0 lots diamond)
--- Generate.sh
+++ Generate.sh
@@ -2,24 +2,87 @@
 
 source Config.sh
 
+rm -R build
 mkdir build
-cp data/datapack build/datapack
+cp -r data/datapack build/datapack
 
-for file in build/datapack/LuckyWorld/data/minecraft/loot_tables/*
+itemEntry=\
+$'
+        {
+          "type": "minecraft:item",
+          "weight": {weight},
+          "quality": {quality},
+          "name": "minecraft:diamond",
+          "functions": [
+            {
+              "function": "minecraft:set_count",
+              "count": {count}
+            }
+          ]
+        },'
+lootTableEntry=\
+$'
+        {
+          "type": "minecraft:item",
+          "weight": {weight},
+          "quality": {quality},
+          "name": "minecraft:diamond",
+          "functions": [
+            {
+              "function": "minecraft:set_count",
+              "count": {count}
+            }
+          ]
+        },'
+entryBuffer=""
+for block in ${blocks}
 do
-  echo "Processing $file"
-  sed -i 's/old-text/new-text/g' $file
+  echo "Processing ${block}"
+  cp build/datapack/LuckyWorld/data/minecraft/loot_tables/{block}.json "build/datapack/LuckyWorld/data/minecraft/loot_tables/${block}.json"
+  file="build/datapack/LuckyWorld/data/minecraft/loot_tables/${block}.json"
+  block=${block}[@]
+  block=(${!block})
+  toAdd=""
+  echo 312: ${#block[@]}
+  for (( i=0; i<$(( ${#block[@]} / 3 )); ++i)) # repeat for each block drop
+  do
+    echo 123: $i + ${block[(( $i * 3 ))]}
+    weight=${block[(( $i * 3 - 2 ))]}_weight[@]
+    quality=${block[(( $i * 3 - 2 ))]}_quality[@]
+    count=${block[(( $i * 3 - 2 ))]}_count[@]
+    for (( n=0; n<${#weight[@]}; ++n))
+    do
+      if [ ${block[(( $i * 3 - 3 ))]} == 0 ]
+      then
+        entryBuffer+=$(sed "s/{weight}/${!type}/g" ${itemEntry})
+        entryBuffer+=$(sed "s/{quality}/${!type}/g" ${entryBuffer})
+        toAdd+=$(sed "s/{count}/${!type}/g" ${entryBuffer})
+      elif [ ${block[(( $i * 3 - 3 ))]} -eq 1 ]
+      then
+        entryBuffer+=$(sed "s/{weight}/${!type}/g" ${lootTableEntry})
+        entryBuffer+=$(sed "s/{quality}/${!type}/g" ${entryBuffer})
+        toAdd+=$(sed "s/{count}/${!type}/g" ${entryBuffer})
+      else
+        echo something broke
+      fi
+    done
+  done
+  sed -i "s/{entries}/${toAdd::-1}/g" $file
 done
+rm build/datapack/LuckyWorld/data/minecraft/loot_tables/{block}.json
 
-for material in equip_materials
+for material in ${equip_materials[@]}
 do
-  echo "Processing ${!material}"
-  cp "build/datapack/LuckyWorld/data/lucky_loot/loot_tables/{material}_equip.json" "build/datapack/LuckyWorld/data/lucky_loot/loot_tables/${!material}_equip.json"
-  file="build/datapack/LuckyWorld/data/lucky_loot/loot_tables/${!material}_equip.json"
-  sed -i "s/{tool_material}/${!material[1]}/g" $file
-  sed -i "s/{armor_material}/${!material[2]}/g" $file
-  sed -i "s/{level}/${!material[3]}/g" $file
-  sed -i "s/{chance}/${!material[4]}/g" $file
+  echo "Processing ${material}"
+  cp build/datapack/LuckyWorld/data/lucky_equip/loot_tables/{material}_equip.json "build/datapack/LuckyWorld/data/lucky_equip/loot_tables/${material}.json"
+  file="build/datapack/LuckyWorld/data/lucky_equip/loot_tables/${material}.json"
+  material=${material}[@]
+  material=(${!material})
+  sed -i "s/{tool_material}/${material[1]}/g" $file
+  sed -i "s/{armor_material}/${material[2]}/g" $file
+  sed -i "s/{level}/${material[3]}/g" $file
+  sed -i "s/{chance}/${material[4]}/g" $file
 done
+rm build/datapack/LuckyWorld/data/lucky_equip/loot_tables/{material}_equip.json
 
-zip LuckyWorld.zip /build/datapack/
+zip LuckyWorld.zip build/datapack/

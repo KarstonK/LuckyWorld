f8c6b7cfb02a3cdfbea64a1504006b990a0a88e7
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-04-22 14:35:16 -0400

    Hopefully fully added progressive items support
    Fortune might not work

--- Generate.sh
+++ Generate.sh
@@ -51,37 +51,34 @@ do
   block=(${!block})
   toAdd=''
   echo 312: ${#block[@]}
-  for (( i=0; i<$(( ${#block[@]} / 3 )); ++i)) # repeat for each block drop
+  for (( i=0; i<$(( ${#block[@]} / 4 )); ++i)) # repeat for each block drop
   do
-    echo 123: $i + ${block[(( $i * 3 ))]}
-    weight=${block[(( $i * 3 - 2 ))]}_weight[@]
-    quality=${block[(( $i * 3 - 2 ))]}_quality[@]
-    count=${block[(( $i * 3 - 2 ))]}_count[@]
+    echo 123: $i + ${block[(( $i * 4 ))]}
+    weight=${block[(( $i * 4 - 2 ))]}_weight[@]
+    quality=${block[(( $i * 4 - 2 ))]}_quality[@]
+    count=${block[(( $i * 4 - 2 ))]}_count[@]
     for (( n=0; n<${#weight[@]}; ++n)) # repeat for each weighting of the drop
     do
-      if [ ${block[(( $i * 3 - 3 ))]} == 0 ]
+      if [ ${block[(( $i * 4 - 3 ))]} == 0 ]
       then
         if [ ${!count} == 1 ]
         then
           entryBuffer=${itemEntrySingle}
         else
           entryBuffer=${itemEntry}
+          entryBuffer=$(sed "s/{count}/${!count}/g" <<< ${entryBuffer})
         fi
-        entryBuffer=$(sed "s/{weight}/${!weight}/g" ${entryBuffer})
+        entryBuffer=$(sed "s/{weight}/${!weight}/g" <<< ${entryBuffer})
         if [ ${!quality} != 0 ]
         then
-          entryBuffer=$(sed "s/{quality}/${!quality}/g" ${entryBuffer})
-        fi
-        if [ ${!count} != 1 ]
-        then
-          entryBuffer=$(sed "s/{count}/${!count}/g" ${entryBuffer})
+          entryBuffer=$(sed "s/{quality}/${!quality}/g" <<< ${entryBuffer})
         fi
         toAdd+=${entryBuffer}
-      elif [ ${block[(( $i * 3 - 3 ))]} -eq 1 ]
+      elif [ ${block[(( $i * 4 - 3 ))]} -eq 1 ]
       then
-        entryBuffer+=$(sed "s/{weight}/${!type}/g" ${lootTableEntry})
-        entryBuffer+=$(sed "s/{quality}/${!type}/g" ${entryBuffer})
-        toAdd+=$(sed "s/{count}/${!type}/g" ${entryBuffer})
+        entryBuffer+=$(sed "s/{weight}/${!type}/g" <<< ${lootTableEntry})
+        entryBuffer+=$(sed "s/{quality}/${!type}/g" <<< ${entryBuffer})
+        toAdd+=$(sed "s/{count}/${!type}/g" <<< ${entryBuffer})
       else
         echo something broke
       fi
@@ -104,5 +101,29 @@ do
   sed -i "s/{chance}/${material[5]}/g" $file
 done
 rm build/datapack/LuckyWorld/data/lucky_equip/loot_tables/{material}_equip.json
+
+for item in ${progressive_items[@]}
+do
+  echo "Processing ${item}"
+  cp build/datapack/LuckyWorld/data/lucky_items/loot_tables/{item}.json "build/datapack/LuckyWorld/data/lucky_items/loot_tables/${item}.json"
+  file="build/datapack/LuckyWorld/data/lucky_items/loot_tables/${item}.json"
+  item=${item}[@]
+  item=(${!item})
+  for (( i=0; i<${#block[@]}; ++i))
+  do
+    if [ ${!count} == 1 ]
+    then
+      entryBuffer=${itemEntrySingle}
+    else
+      entryBuffer=${itemEntry}
+      entryBuffer=$(sed "s/{count}/${progressive_count[$i]}/g" <<< ${entryBuffer})
+    fi
+    entryBuffer=$(sed "s/{weight}/${progressive_weight[$i]}/g" <<< ${entryBuffer})
+    entryBuffer=$(sed "s/{quality}/${progressive_quality[$i]}/g" <<< ${entryBuffer})
+    toAdd+=${entryBuffer}
+  done
+  sed -i "s/{entries}/${toAdd::-1}/g" $file
+done
+rm build/datapack/LuckyWorld/data/lucky_items/loot_tables/{item}.json
 
 zip LuckyWorld.zip build/datapack/

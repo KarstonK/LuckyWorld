c91f2c75474c90e33af11121bf42e50d069f1163
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-04-22 10:31:27 -0400

    Updated Generate.sh
    Added more support for custom models, and slightly optimized it on Minecraft's end when there is only 1 item, or 0 quality

--- Generate.sh
+++ Generate.sh
@@ -6,6 +6,14 @@ rm -R build
 mkdir build
 cp -r data/datapack build/datapack
 
+itemEntrySingle=\
+$'
+        {
+          "type": "minecraft:item",
+          "weight": {weight},
+          "quality": {quality},
+          "name": "minecraft:diamond"
+        },'
 itemEntry=\
 $'
         {
@@ -34,7 +42,6 @@ $'
             }
           ]
         },'
-entryBuffer=""
 for block in ${blocks}
 do
   echo "Processing ${block}"
@@ -42,7 +49,8 @@ do
   file="build/datapack/LuckyWorld/data/minecraft/loot_tables/${block}.json"
   block=${block}[@]
   block=(${!block})
-  toAdd=""
+  toAdd=''
+  entryBuffer=''
   echo 312: ${#block[@]}
   for (( i=0; i<$(( ${#block[@]} / 3 )); ++i)) # repeat for each block drop
   do
@@ -50,13 +58,25 @@ do
     weight=${block[(( $i * 3 - 2 ))]}_weight[@]
     quality=${block[(( $i * 3 - 2 ))]}_quality[@]
     count=${block[(( $i * 3 - 2 ))]}_count[@]
-    for (( n=0; n<${#weight[@]}; ++n))
+    for (( n=0; n<${#weight[@]}; ++n)) # repeat for each weighting of the drop
     do
       if [ ${block[(( $i * 3 - 3 ))]} == 0 ]
       then
-        entryBuffer+=$(sed "s/{weight}/${!type}/g" ${itemEntry})
-        entryBuffer+=$(sed "s/{quality}/${!type}/g" ${entryBuffer})
-        toAdd+=$(sed "s/{count}/${!type}/g" ${entryBuffer})
+        if [ ${!count} == 1 ]
+        then
+          entryBuffer+=${itemEntrySingle}
+        else
+          entryBuffer+=${itemEntry}
+        fi
+        entryBuffer=$(sed "s/{weight}/${!weight}/g" ${entryBuffer})
+        if [ ${!quality} != 0 ]
+        then
+          entryBuffer=$(sed "s/{quality}/${!quality}/g" ${entryBuffer})
+        fi
+        if [ ${!count} != 1 ]
+        then
+          toAdd=$(sed "s/{count}/${!count}/g" ${entryBuffer})
+        fi
       elif [ ${block[(( $i * 3 - 3 ))]} -eq 1 ]
       then
         entryBuffer+=$(sed "s/{weight}/${!type}/g" ${lootTableEntry})
@@ -78,10 +98,10 @@ do
   file="build/datapack/LuckyWorld/data/lucky_equip/loot_tables/${material}.json"
   material=${material}[@]
   material=(${!material})
-  sed -i "s/{tool_material}/${material[1]}/g" $file
+  sed -i "s/{tool_material}/${material[0]}/g" $file
   sed -i "s/{armor_material}/${material[2]}/g" $file
-  sed -i "s/{level}/${material[3]}/g" $file
-  sed -i "s/{chance}/${material[4]}/g" $file
+  sed -i "s/{level}/${material[4]}/g" $file
+  sed -i "s/{chance}/${material[5]}/g" $file
 done
 rm build/datapack/LuckyWorld/data/lucky_equip/loot_tables/{material}_equip.json
 
